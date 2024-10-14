322e38a55aa6b5b83f68700964bef326ba443253
Daria Kolmuk <nross.dasha@bk.ru>
2024-10-14 22:53:59 +0300

    lesso 1: make H

--- Лекция 1/Практические задания/!ProgramTasks/H/Program.cs
+++ Лекция 1/Практические задания/!ProgramTasks/H/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace H
+﻿using System;
+
+namespace H
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Print(GetSquare(42));
+            {
+                Print(GetSquare(42));
+            }
+            static void Print(int x)
+            {
+                Console.WriteLine(x);
+            }
+            static int GetSquare(int x)
+            {
+                return (int)Math.Pow(x, 2);
+            }
+            Console.ReadKey();
         }
     }
 }
