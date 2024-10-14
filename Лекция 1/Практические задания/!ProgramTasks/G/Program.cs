8cb44b84c9642649dc0a48d7431999928cb5e103
Daria Kolmuk <nross.dasha@bk.ru>
2024-10-14 22:51:18 +0300

    lesson 1: make G

--- Лекция 1/Практические задания/!ProgramTasks/G/Program.cs
+++ Лекция 1/Практические задания/!ProgramTasks/G/Program.cs
@@ -5,7 +5,7 @@
         private static string GetGreetingMessage(string name, double salary)
         {
             // возвращает "Hello, <name>, your salary is <salary>"
-            ...
+            return $"Hello, {name}, your salary is {salary}";
         }
 
         static void Main(string[] args)
@@ -13,6 +13,7 @@
             Console.WriteLine(GetGreetingMessage("Student", 10.01));
             Console.WriteLine(GetGreetingMessage("Bill Gates", 10000000.5));
             Console.WriteLine(GetGreetingMessage("Steve Jobs", 1));
+            Console.ReadKey();
         }
     }
 }
