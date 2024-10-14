8c3fb12b1fb3962f99a72beefd55886cff8c392b
Daria Kolmuk <nross.dasha@bk.ru>
2024-10-14 23:11:27 +0300

    lesson 1: make I

--- Лекция 1/Практические задания/!ProgramTasks/I/Program.cs
+++ Лекция 1/Практические задания/!ProgramTasks/I/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace I
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace I
 {
     internal class Program
     {
         static string GetLastHalf(string text)
         {
-            ...
+            text = text.Substring(text.Length / 2);
+            string c = text.Replace(" ", string.Empty);
+            return c;
         }
 
         static void Main(string[] args)
@@ -12,6 +17,7 @@
             Console.WriteLine(GetLastHalf("I love CSharp!"));
             Console.WriteLine(GetLastHalf("1234567890"));
             Console.WriteLine(GetLastHalf("до ре ми фа соль ля си"));
+            Console.ReadKey();
         }
     }
 }
